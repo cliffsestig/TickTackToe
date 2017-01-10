7f4f0e49ab32ab58baacf156f7769721235cea0e
larsheesakkers1 <larsheesakkers@live.nl>
2017-01-10 13:03:31 +0100

    design mainactivity
    design mainactivity(buttons, animation)

--- app/src/main/java/com/example/cliffsestig/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/cliffsestig/tictactoe/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.cliffsestig.tictactoe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.TranslateAnimation;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RunAnimation();
     }
+    private void RunAnimation()
+    {
+
+        TextView tv = (TextView) findViewById(R.id.txt_title);
+
+        TranslateAnimation cloud_moving = new TranslateAnimation(
+                Animation.ABSOLUTE, 200,
+                Animation.ABSOLUTE, 10,
+                Animation.ABSOLUTE, 0,
+                Animation.ABSOLUTE, 0
+        );
+
+        cloud_moving.setDuration(2000);
+        cloud_moving.setFillAfter(true);
+        cloud_moving.setStartOffset(1000);
+        cloud_moving.setRepeatCount(Animation.INFINITE);
+        cloud_moving.setRepeatMode(Animation.REVERSE);
+        tv.startAnimation(cloud_moving);
+    }
+
 }
