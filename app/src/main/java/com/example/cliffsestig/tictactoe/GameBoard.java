b7d37cfb079f246c227254f012b9679766e9f519
TigerzHead <cliffsestig@gmail.com>
2017-01-12 18:28:52 +0100

    Added gameboard

--- app/src/main/java/com/example/cliffsestig/tictactoe/GameBoard.java
+++ app/src/main/java/com/example/cliffsestig/tictactoe/GameBoard.java
@@ -6,30 +6,30 @@ package com.example.cliffsestig.tictactoe;
 
 public class GameBoard {
     //Determines if there is a winner or not checks each diagonal then loops through each row/column
-    public boolean isWinner() {
-        // Check Diagonals
-        if (theBoard[0][0] == theBoard[1][1] && theBoard[0][0] == theBoard[2][2] && theBoard[0][0] != "")
-
-            return true;
-
-        if (theBoard[2][0] == theBoard[1][1] && theBoard[2][0] == theBoard[0][2] && theBoard[2][0] != "")
-
-            return true;
-
-        for (int i = 0; i < 3; i++) {
-
-// Check Rows
-            if (theBoard[i][0] == theBoard[i][1] && theBoard[i][1] == theBoard[i][2] && theBoard[i][0] != "")
-
-                return true;
-
-// Check Columns
-            if (theBoard[0][i] == theBoard[1][i] && theBoard[1][i] == theBoard[2][i] && theBoard[0][i] != "")
-
-                return true;
-
-        }
-
-        return false;
-    }
+//    public boolean isWinner() {
+//        // Check Diagonals
+//        if (theBoard[0][0] == theBoard[1][1] && theBoard[0][0] == theBoard[2][2] && theBoard[0][0] != "")
+//
+//            return true;
+//
+//        if (theBoard[2][0] == theBoard[1][1] && theBoard[2][0] == theBoard[0][2] && theBoard[2][0] != "")
+//
+//            return true;
+//
+//        for (int i = 0; i < 3; i++) {
+//
+//// Check Rows
+//            if (theBoard[i][0] == theBoard[i][1] && theBoard[i][1] == theBoard[i][2] && theBoard[i][0] != "")
+//
+//                return true;
+//
+//// Check Columns
+//            if (theBoard[0][i] == theBoard[1][i] && theBoard[1][i] == theBoard[2][i] && theBoard[0][i] != "")
+//
+//                return true;
+//
+//        }
+//
+//        return false;
+//    }
 }
